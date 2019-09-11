191cc7bd8afe579353c406b79d16b2b1058a71af
Kristina Taing <kataing12@gmail.com>
2019-09-10 21:11:35 -0700

    [prog] added mobile menu bar

--- client/src/components/menu.jsx
+++ client/src/components/menu.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import { FaBars } from 'react-icons/fa';
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  handleOnClick = (e) => {
+    console.log(e.target.value);
+    console.log('click');
+  }
+
   render() {
     return (
       <section className='menu'>
         <div className='menu-item-container'>
+          <FaBars className='menu-mobile' onClick={this.handleOnClick}/>
+          <a href='#main' className='menu-item'>Home</a>
           <a href='#about-me' className='menu-item'>About Me</a>
           <a href='#highlights' className='menu-item'>Highlights</a>
           <a href='#applications' className='menu-item'>Applications</a>
