b188e3e962cf29724323935aad4ff9ea37dd3f4f
Kristina Taing <kataing12@gmail.com>
2019-09-13 21:59:05 -0700

    [feat] added welcome text to main section

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -3,12 +3,40 @@ import React from 'react';
 class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      fullText: "Welcome to Kristina Taing's Portfolio!",
+      displayLength: 0  // Max length is 38
+    }
+  }
+
+  addLetter() {
+    if (this.state.displayLength < 38) {
+      this.setState({
+        displayLength: this.state.displayLength + 1
+      }, () => {
+        console.log(this.state.displayLength);
+        setInterval(this.addLetter(), 1000);
+      })
+    }
+  }
+
+  componentDidMount() {
+    this.addLetter();
   }
 
   render() {
     return (
       <div id='main' className='main'>
-        This is main
+        <div></div>
+        <div className='typewriter-container'>
+          <span className='intro-text'>
+            {this.state.fullText.slice(0, this.state.displayLength)}
+          </span>
+          <span className='typewriter'></span>
+        </div>
+        <a href='#about-me' className='down-arrow-container'>
+          <img className='down-arrow' src='down-arrow.png' alt='down-arrow'/>
+        </a>
       </div>
     )
   }
