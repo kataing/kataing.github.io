048d73097bf68d2c82c8b747cda66fe4eee00304
Kristina Taing <kataing12@gmail.com>
2019-09-16 15:39:06 -0700

    [style] changed components withouth states to be state-less components

--- client/src/components/applications.jsx
+++ client/src/components/applications.jsx
@@ -8,13 +8,14 @@ class Applications extends React.Component {
   render() {
     return (
       <section id='applications' className='section'>
-         <h1 className='section-header-container'>
-        <span className='section-separator'></span>
+        <h1 className='section-header-container'>
+          <span className='section-separator'></span>
           <div className='section-header'>
             Applications
           </div>
           <span className='section-separator'></span>
         </h1>
+        
       </section>
     )
   }
