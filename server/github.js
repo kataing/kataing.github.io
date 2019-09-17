6db601715fa52b1ab0dce5b26c00a3a54c8e1209
Kristina Taing <kataing12@gmail.com>
2019-09-17 13:54:13 -0700

    [feat] wrote helper that will be used to make call to github api

---
+++ server/github.js
@@ -0,0 +1,14 @@
+const request = require('request');
+const config = require('../config.js');
+
+let getReposByUsername = (username, cb) => {
+  let options = {
+    url: `https://api.github.com/users/${username}/repos`,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${config.TOKEN}`
+    }
+  };
+}
+
+module.exports.getReposByUsername = getReposByUsername;
