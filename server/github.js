00fa0f358d9fdb19e3a8310f10ce961aa30d3c7a
Kristina Taing <kataing12@gmail.com>
2019-09-17 15:22:27 -0700

    [prog] wrote get request to github api for all repos

--- server/github.js
+++ server/github.js
@@ -1,14 +1,23 @@
 const request = require('request');
-const config = require('../config.js');
+const { TOKEN } = require('../config.js');
+const { username } = require('../config.js');
 
-let getReposByUsername = (username, cb) => {
+let getReposByUsername = (cb) => {
   let options = {
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${TOKEN}`
     }
   };
+
+  request(options, (err, data) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, data);
+    }
+  });
 }
 
 module.exports.getReposByUsername = getReposByUsername;
