68f643ec2346dc07ee5c1c77d3a7101e41893a2e
Kristina Taing <kataing12@gmail.com>
2019-09-17 20:42:49 -0700

    [prog] updated token and username to access enviroment variables or values in the gitignored file

--- server/github.js
+++ server/github.js
@@ -2,12 +2,22 @@ const request = require('request');
 const { TOKEN } = require('../config.js');
 const { username } = require('../config.js');
 
+let KEY = process.env.KEY;
+if (KEY == null || KEY == "") {
+  KEY = TOKEN;
+}
+
+let login = process.env.login;
+if (login == null || login == "") {
+  login = username;
+}
+
 let getReposByUsername = (cb) => {
   let options = {
-    url: `https://api.github.com/users/${username}/repos`,
+    url: `https://api.github.com/users/${login}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${TOKEN}`
+      'Authorization': `token ${KEY}`
     }
   };
 
