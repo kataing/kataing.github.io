244d6f33522f75063cc48d98d7f085c0ecd3bbf3
Kristina Taing <kataing12@gmail.com>
2019-09-17 20:12:41 -0700

    [prog] updated app to listen to correct port on heroku and local machine

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const parser = require('body-parser');
 const github = require('./github.js');
 
 const app = express();
-const port = 3000;
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
