2fbb8ad8f48690ca4239eb209e5bc5b786866f4e
Kristina Taing <kataing12@gmail.com>
2019-09-21 17:43:49 -0700

    [feat] added a loading button, so users can load more or less repos to the page

--- client/src/components/allRepos.jsx
+++ client/src/components/allRepos.jsx
@@ -7,8 +7,27 @@ class AllRepos extends React.Component {
     super(props);
 
     this.state = {
-      repos: []
+      allRepos: [],
+      repos: [],
+      length: 8
     }
+  }
+
+  handleOnClick = (e) => {
+    var newLength = this.state.length;
+    if(e.target.className === 'repo-button show-more') {
+      if (newLength < this.state.allRepos.length) {
+        newLength += 8;
+      }
+    } else if (e.target.className === 'repo-button show-less') {
+      if (newLength > 8) {
+        newLength -= 8;
+      }
+    }
+    this.setState({
+      repos: this.state.allRepos.slice(0, newLength),
+      length: newLength
+    })
   }
 
   getRepos = () => {
@@ -16,7 +35,8 @@ class AllRepos extends React.Component {
       .get('/repos')
       .then(({ data }) => {
         this.setState({
-          repos: data
+          allRepos: data,
+          repos: data.slice(0, this.state.length)
         })
       })
       .catch(() => console.log('We were not able to complete your request.'))
@@ -32,7 +52,7 @@ class AllRepos extends React.Component {
         <h1 className='section-header-container'>
           <span className='section-separator'></span>
           <div className='section-header'>
-            All Repos
+            Repos
           </div>
           <span className='section-separator'></span>
         </h1>
@@ -42,6 +62,10 @@ class AllRepos extends React.Component {
               <Repo repo={repo} key={key} />
             )
           })}
+        </div>
+        <div className='repo-button-container'>
+          {this.state.length > 8 && <button className='repo-button show-less' onClick={this.handleOnClick}>Show less repos</button>}
+          {this.state.length < this.state.allRepos.length && <button className='repo-button show-more' onClick={this.handleOnClick}>Show more repos</button>}
         </div>
       </section>
     )
