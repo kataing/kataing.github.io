96093905e80b02b8418a25dd2dcb774efc3f1f51
Kristina Taing <kataing12@gmail.com>
2019-09-23 11:01:07 -0700

    [feat] added resume to portfolio

--- client/src/components/menu.jsx
+++ client/src/components/menu.jsx
@@ -25,6 +25,7 @@ class Menu extends React.Component {
             <a href='#about-me' className='menu-item-mobile' onClick={this.handleMobileMenuOnClick} >About Me</a>
             <a href='#applications' className='menu-item-mobile' onClick={this.handleMobileMenuOnClick} >Applications</a>
             <a href='#allRepos' className='menu-item-mobile' onClick={this.handleMobileMenuOnClick} >Repos</a>
+            <a href='/images/Resume_Kristina Taing.pdf' target='_blank' className='menu-item-mobile' onClick={this.handleMobileMenuOnClick} >Resume</a>
           </div>
       )
     }
@@ -71,6 +72,7 @@ class Menu extends React.Component {
             <a href='#about-me' className='menu-item'>About Me</a>
             <a href='#applications' className='menu-item'>Applications</a>
             <a href='#allRepos' className='menu-item'>Repos</a>
+            <a href='/images/Resume_Kristina Taing.pdf' target='_blank' className='menu-item'>Resume</a>
           </div>
           {this.displayMobileMenu()}
           <div className='social-icon-container'>
