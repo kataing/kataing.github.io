81b33a377631cda2c9aa194709c6ab8cca3d0f26
Kristina Taing <kataing12@gmail.com>
2019-09-23 13:42:13 -0700

    [feat] added a loading page with styling

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -5,7 +5,9 @@ class Main extends React.Component {
     super(props);
     this.state = {
       fullText: "Welcome to Kristina Taing's Portfolio!",
-      displayLength: 0
+      displayLength: 0,
+      loading: true,
+      loadedImages: 0
     }
     this.addLetter = this.addLetter.bind(this);
   }
@@ -20,19 +22,47 @@ class Main extends React.Component {
     })
   }
 
-  componentDidMount() {
-    this.addLetter();
+  handleImageLoad = () => {
+    this.setState({
+      loadedImages: this.state.loadedImages + 1
+    }, () => {
+      if(this.state.loadedImages === 2) {
+        this.setState({
+          loading: false
+        }, () => {
+          this.addLetter();
+        })
+      }
+    })
+  }
+
+  handleImageError = () => {
+    this.setState({
+      loadedImages: 0
+    })
+  }
+
+  displayLoadingPage = () => {
+    if (this.state.loading) {
+      return(
+        <section className='loading-page'>
+          <img className='loading-spinner' src='/images/loading-spinner.gif' alt='loading-spinner' />
+          <span>Just making sure the page is perfect for you</span>
+        </section>
+      )
+    }
   }
 
   render() {
     return (
       <section id='main' className='main'>
+        {this.displayLoadingPage()}
         <div className='image-container-desktop'>
-          <img className='portfolio-image' src='images/portfolio-image.jpg' alt='portfolio-image' />
+          <img className='portfolio-image' src='images/portfolio-image.jpg' onLoad={this.handleImageLoad} onError={this.handleImageError} alt='portfolio-image' />
         </div>
         <div className='main-container'>
           <div className='image-container-mobile'>
-            <img className='portfolio-image' src='images/portfolio-image.jpg' alt='portfolio-image' />
+            <img className='portfolio-image' src='images/portfolio-image.jpg' onLoad={this.handleImageLoad} onError={this.handleImageError} alt='portfolio-image' />
           </div>
           <div className='typewriter-container'>
             <span className='intro-text'>
